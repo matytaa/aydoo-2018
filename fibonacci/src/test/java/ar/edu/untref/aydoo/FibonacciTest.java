ab2629c78423739c1864913be6ac474998a2f324
Matias Nievas Soto <matytaa@gmail.com>
2018-03-26 21:48:17 -0300

    Agregando Test

--- fibonacci/src/test/java/ar/edu/untref/aydoo/FibonacciTest.java
+++ fibonacci/src/test/java/ar/edu/untref/aydoo/FibonacciTest.java
@@ -12,60 +12,60 @@ public class FibonacciTest {
 	Fibonacci miFibonacci = new Fibonacci();
 	int resultado = 0;
 	
-	@Ignore
+	@Test
 	public void fibonacciDe1() {
 		resultado = miFibonacci.ejecutarFibonacci(1);
 		Assert.assertEquals(1, resultado);
 	}
 	
-	@Ignore
+	@Test
 	public void fibonacciDe2() {
 		resultado = miFibonacci.ejecutarFibonacci(2);
 		Assert.assertEquals(1, resultado);
 	}
 	
-	@Ignore
+	@Test
 	public void fibonacciDe3() {
 		resultado = miFibonacci.ejecutarFibonacci(3);
 		Assert.assertEquals(2, resultado);
 	}
 	
-	@Ignore
+	@Test
 	public void fibonacciDe4() {
 		resultado = miFibonacci.ejecutarFibonacci(4);
 		Assert.assertEquals(3, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe5() {
 		resultado = miFibonacci.ejecutarFibonacci(5);
 		Assert.assertEquals(5, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe6() {
 		resultado = miFibonacci.ejecutarFibonacci(6);
 		Assert.assertEquals(8, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe7() {
 		resultado = miFibonacci.ejecutarFibonacci(7);
 		Assert.assertEquals(13, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe8() {
 		resultado = miFibonacci.ejecutarFibonacci(8);
 		Assert.assertEquals(21, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe9() {
 		resultado = miFibonacci.ejecutarFibonacci(9);
 		Assert.assertEquals(34, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe10() {
 		resultado = miFibonacci.ejecutarFibonacci(10);
 		Assert.assertEquals(55, resultado);
 	}
-	@Ignore
+	@Test
 	public void fibonacciDe11() {
 		resultado = miFibonacci.ejecutarFibonacci(11);
 		Assert.assertEquals(89, resultado);
