418f3b762c179a9ba4eac77d6de202d77d39ef14
Matias Nievas Soto <matytaa@gmail.com>
2018-04-30 22:15:18 -0300

    Implementando fibonacci en ManejadorFibonacci

--- fibo3/src/main/java/ar/edu/untref/aydoo/ManejadorFibonacci.java
+++ fibo3/src/main/java/ar/edu/untref/aydoo/ManejadorFibonacci.java
@@ -2,9 +2,11 @@ package ar.edu.untref.aydoo;
 
 public class ManejadorFibonacci {
     private final ProcesadorDeParametros miProcesador;
+    private Fibonacci miFibonacci;
 
-    public ManejadorFibonacci(ProcesadorDeParametros unProcesador) {
+    public ManejadorFibonacci(ProcesadorDeParametros unProcesador, Fibonacci unFibonacci) {
         this.miProcesador = unProcesador;
+        this.miFibonacci = unFibonacci;
     }
 
     public void procesarArgumentos(String unArgumento) {
@@ -13,5 +15,14 @@ public class ManejadorFibonacci {
 
     public ProcesadorDeParametros darProcesadorDeParemetros(){
         return this.miProcesador;
+    }
+
+    public int ejecutarFibonnaci(int unNumeroFibonacci) {
+        inicializarFibonacci(unNumeroFibonacci);
+        return miFibonacci.ejecutarFibonacci();
+    }
+
+    private void inicializarFibonacci(int unNumeroFibonacci) {
+        this.miFibonacci = new Fibonacci(unNumeroFibonacci);
     }
 }
--- fibo3/src/test/java/ar/edu/untref/aydoo/ManejadorFibonacciTest.java
+++ fibo3/src/test/java/ar/edu/untref/aydoo/ManejadorFibonacciTest.java
@@ -7,8 +7,17 @@ public class ManejadorFibonacciTest {
     @Test
     public void deberiaPoderSetearParametrosCorrectamente(){
         ProcesadorDeParametros unProcesador = new ProcesadorDeParametros();
-        ManejadorFibonacci miManejador = new ManejadorFibonacci(unProcesador);
+        ManejadorFibonacci miManejador = new ManejadorFibonacci(unProcesador, null);
         miManejador.procesarArgumentos("-m=s");
         Assert.assertTrue(miManejador.darProcesadorDeParemetros().puedoContinuar());
+    }
+
+    @Test
+    public void deberiaPoderEjecutarElFibonacciDe7(){
+
+        Fibonacci unFibonacci = null;
+        ManejadorFibonacci miManejador = new ManejadorFibonacci(null, unFibonacci);
+        int resultado = 13;
+        Assert.assertEquals(resultado, miManejador.ejecutarFibonnaci(7));
     }
 }
