e8bee6952ad059ed8bbd33ac2ab1f1fcf11de29a
Matias Nievas Soto <matytaa@gmail.com>
2018-07-15 16:01:07 -0300

    primer acercamiento a aplicador de impuestos

--- finanzas/src/main/java/ar/edu/untref/aydoo/GrabadorDeImpuesto.java
+++ finanzas/src/main/java/ar/edu/untref/aydoo/GrabadorDeImpuesto.java
@@ -2,10 +2,33 @@ package ar.edu.untref.aydoo;
 
 public class GrabadorDeImpuesto {
 
-    public Double grabarImpuestos(Double monto) {
+    public Double grabarImpuestos(Double monto, boolean esEmpresa) {
+        Double resultado = 0d;
         if (monto < 20000.0d){
-            return monto*1.d;
+            return resultado;
         }
-        return monto*0.05;
+
+        if (esEmpresa) {
+            resultado = grabarImpuestosEmpresa(monto);
+        }else {
+            resultado = grabarImpuestosParticular(monto);
+        }
+        return resultado;
+    }
+
+    private Double grabarImpuestosParticular(Double monto) {
+        Double resultado = 0d;
+        if (monto > 50000d && 100000d > monto)
+            resultado = monto*0.05;
+
+        return resultado;
+    }
+
+    private Double grabarImpuestosEmpresa(Double monto) {
+        Double resultado = 0d;
+        if (monto > 20000d && 50000 > monto)
+            resultado = monto*0.05;
+
+        return resultado;
     }
 }
--- finanzas/src/test/java/ar/edu/untref/aydoo/GrabadorDeImpuestoTest.java
+++ finanzas/src/test/java/ar/edu/untref/aydoo/GrabadorDeImpuestoTest.java
@@ -4,48 +4,76 @@ import ar.edu.untref.aydoo.excepciones.InversionException;
 import org.junit.Test;
 import org.junit.Assert;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class GrabadorDeImpuestoTest {
     @Test
     public void deberiaDevolverElMismoMontoEnviadoYaQueNoAplicaImpuesto() {
-        GrabadorDeImpuesto gabadorDeFinanzas = new GrabadorDeImpuesto();
-        Double result = gabadorDeFinanzas.grabarImpuestos(10d);
-        Assert.assertEquals(10d, result, 0.001);
+        GrabadorDeImpuesto grabadorDeFinanzas = new GrabadorDeImpuesto();
+        Double result = grabadorDeFinanzas.grabarImpuestos(10d, true);
+        Assert.assertEquals(0d, result, 0.001);
     }
 
     @Test
-    public void obtengo25KDeGananciasComprandoDolares() throws InversionException {
+    public void obtengo5KDeGananciasComprandoDolaresYNoAplicaImpuesto() throws InversionException {
         Double monto = 20000d;
         Double cotizacionInicial = 20d;
         Double cotizacionFinal = 25d;
 
         CompraDolares compraDolares = new CompraDolares(monto, cotizacionInicial, cotizacionFinal);
-        GrabadorDeImpuesto gabadorDeFinanzas = new GrabadorDeImpuesto();
-
-        Double result = gabadorDeFinanzas.grabarImpuestos(compraDolares.obtenerGanacias());
-        Assert.assertEquals(5000d, result, 0.001);
+        GrabadorDeImpuesto grabadorDeFinanzas = new GrabadorDeImpuesto();
+        Double result = grabadorDeFinanzas.grabarImpuestos(compraDolares.obtenerGanacias(), false);
+        Assert.assertEquals(0d, result, 0.001);
     }
 
     @Test
-    public void obtengo25KDeGananciasPlazoFijo() throws InversionException {
+    public void obtengo2KDeGananciasPlazoFijoYNoAplicoImpuesto() throws InversionException {
         Double monto = 20000d;
         int plazoAcordado = 90;
         Double interes = 10d;
+
         PlazoFijo plazoFijoTradicional = new PlazoFijo(monto, plazoAcordado, interes);
-        GrabadorDeImpuesto gabadorDeFinanzas = new GrabadorDeImpuesto();
-        Double result = gabadorDeFinanzas.grabarImpuestos(plazoFijoTradicional.obtenerGanacias());
-        Assert.assertEquals(2000d, result, 0.001);
+        GrabadorDeImpuesto grabadorDeFinanzas = new GrabadorDeImpuesto();
+
+        Double result = grabadorDeFinanzas.grabarImpuestos(plazoFijoTradicional.obtenerGanacias(), false);
+        Assert.assertEquals(0d, result, 0.001);
     }
 
     @Test
-    public void obtengo25KDeGananciasPlazoFijoPrecancelable() throws InversionException {
+    public void obtengo1KDeGananciasPlazoFijoPrecancelableYNoAplicoImpuesto() throws InversionException {
         Double monto = 20000d;
         int plazoAcordado = 180;
         int plazoReal = 90;
         Double interes = 10d;
+
         PlazoFijoPrecancelable plazoFijoPrecancelable = new PlazoFijoPrecancelable(monto, plazoAcordado, plazoReal, interes);
-        GrabadorDeImpuesto gabadorDeFinanzas = new GrabadorDeImpuesto();
-        Double result = gabadorDeFinanzas.grabarImpuestos(plazoFijoPrecancelable.obtenerGanacias());
-        Assert.assertEquals(1000d, result, 0.001);
+        GrabadorDeImpuesto grabadorDeFinanzas = new GrabadorDeImpuesto();
+
+        Double result = grabadorDeFinanzas.grabarImpuestos(plazoFijoPrecancelable.obtenerGanacias(), false);
+        Assert.assertEquals(0d, result, 0.001);
+    }
+
+    @Test
+    public void deberiaAplicarEl5PorcientoDeImpuestoPorSerEmpresa(){
+        Inversor unInversor = mock(Inversor.class);
+        when(unInversor.esEmpresa()).thenReturn(true);
+        when(unInversor.calcularGanacias()).thenReturn(30000d);
+        
+        GrabadorDeImpuesto grabadorDeImpuesto = new GrabadorDeImpuesto();
+        Double resultado = grabadorDeImpuesto.grabarImpuestos(unInversor.calcularGanacias(), unInversor.esEmpresa());
+        Assert.assertEquals(1500d, resultado, 0.001);
+    }
+
+    @Test
+    public void deberiaAplicarEl5PorcientoDeImpuestoPorSerParticular(){
+        Inversor unInversor = mock(Inversor.class);
+        when(unInversor.esEmpresa()).thenReturn(false);
+        when(unInversor.calcularGanacias()).thenReturn(60000d);
+
+        GrabadorDeImpuesto grabadorDeImpuesto = new GrabadorDeImpuesto();
+        Double resultado = grabadorDeImpuesto.grabarImpuestos(unInversor.calcularGanacias(), unInversor.esEmpresa());
+        Assert.assertEquals(3000d, resultado, 0.001);
     }
 }
